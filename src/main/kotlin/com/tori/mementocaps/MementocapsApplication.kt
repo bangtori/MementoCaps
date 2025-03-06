588df081eaba906b24d0d68896a6d7c7e4ecc135
Tori <ttang1135@naver.com>
2025-03-06 10:53:13 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/tori/mementocaps/MementocapsApplication.kt
@@ -0,0 +1,11 @@
+package com.tori.mementocaps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MementocapsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MementocapsApplication>(*args)
+}
---
+++ src/test/kotlin/com/tori/mementocaps/MementocapsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tori.mementocaps
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MementocapsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
